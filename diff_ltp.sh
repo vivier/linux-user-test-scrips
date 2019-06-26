05527a06b080554aa5648130e9b3e6dd5d579df3
Laurent Vivier <laurent@vivier.eu>
2019-06-26 12:53:50 -0400

    diff_ltp: add script to comare LTP results
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

---
+++ diff_ltp.sh
@@ -0,0 +1,26 @@
+. ./targets.conf
+
+PREVIOUS=${PREVIOUS:-previous}
+LATEST=${LATEST:-latest}
+for release in $RELEASES ; do
+    for arch in $(eval echo \${QEMU_ARCHS_$release}) ; do
+
+	case $arch in
+	ppc)         arch=powerpc    ;;
+	ppc64le)     arch=ppc64el    ;;
+	sparc32plus) arch=sparc      ;;
+	arm)         arch=armhf      ;;
+	armeb)       arch=armel      ;;
+	aarch64)     arch=arm64      ;;
+	x86_64)      arch=amd64      ;;
+	esac
+
+        if [ "$arch" = "m68k" -a "$release" = "etch" ] ; then
+                release="etch-m68k"
+        fi
+
+        echo "diff $release $arch $TAG"
+	dir=archive/$arch/$release
+	diff -u $dir/$PREVIOUS/results/* $dir/$LATEST/results/*
+    done
+done
